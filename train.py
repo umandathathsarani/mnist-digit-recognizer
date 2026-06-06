2aab50293df8a2938d2bca6f19914b700610771e
Umanda Thathsarani <umathathsarani2003@gmail.com>
2026-06-06 13:04:10 +0530

    Implement CNN model for MNIST digit recognition

--- train.py
+++ train.py
@@ -1,1 +1,25 @@
-# MNIST Training Script
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = models.Sequential([
+    layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test,  y_test, verbose=2)
+
+model.save('mnist_model.keras')
